534f7b457a013ff8dbc6c8423c7e8be8ba1d1b0f
Nguyễn Minh Kha <nmkha19@clc.fitus.edu.vn>
2021-11-14 20:40:29 +0700

    Add DoanhThu

--- Project/Project/doanhthu.py
+++ Project/Project/doanhthu.py
@@ -13,11 +13,21 @@ class DoanhThu(QWidget):
         QWidget.__init__(self)
         self.ui = Ui_Form()
         self.ui.setupUi(self)
+        self._year = 0
         #########################
         ### chỉ cho phép nhập chữ số của năm
         self.onlyInt = QIntValidator()
         self.ui.nam_lnedit.setValidator(self.onlyInt)
         #########################
-        # button event
-
-        #######################
+        self.ui.table.setColumnCount(3)
+        self.ui.table.setRowCount(0)
+        # set column headers name for table "doanh thu"
+        self.ui.table.setHorizontalHeaderItem(0,QTableWidgetItem("Thang"))        
+        self.ui.table.setHorizontalHeaderItem(1,QTableWidgetItem("So Luong Hoa Don"))
+        self.ui.table.setHorizontalHeaderItem(2,QTableWidgetItem("Doanh Thu"))
+    def populateTable(self,hd):
+        self.ui.table.insertRow(self.ui.table.rowCount())
+        for i in range (len(hd)):           
+            item = QTableWidgetItem(str(hd[i]))
+            item.setTextAlignment(Qt.AlignCenter)
+            self.ui.table.setItem(self.ui.table.rowCount()-1,i,item)
